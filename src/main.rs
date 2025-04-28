00c16b265db21f563b7d0df1d1e3acf9b4100ac6
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-04-28 23:29:28 +0700

    Persisted storage i suppose?

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,57 @@
 #![allow(non_snake_case)]
 
+use bytes::Bytes;
 use mini_redis::{Connection, Frame, Command, Command::Get, Command::Set};
 use tokio::net::{TcpListener, TcpStream};
-use std::collections::HashMap;
-
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
 #[tokio::main]
 async fn main() {
-    // let x = TcpListener::bind("0.0.0.0:6379").await.unwrap();
-    let x = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let x = TcpListener::bind("0.0.0.0:6379").await.unwrap();
+    // let x = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     println!("Server has started on: http://{}\n", x.local_addr().unwrap());
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let y = x.accept().await;
 
         match y {
             Ok((z, _)) => {
+                let some = db.clone();
+
                 tokio::spawn(async {
-                    process(z).await;
+                    process(z, some).await;
                 });
             },
-            Err(e) => println!("{e}")
+            Err(e) => println!("You fucked up my guy: {e}")
         }
     }
 }
 
-async fn process(socket: TcpStream) {
-    println!("{}\n", socket.local_addr().unwrap());
-    let mut db = HashMap::new();
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Bytes>>>) {
+    println!("Received a request from: {}", socket.local_addr().unwrap());
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut x = db.lock().unwrap();
+                x.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             },
             Get(cmd) => {
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                let x = db.lock().unwrap();
+                if let Some(value) = x.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
                 }
                 else {
                     Frame::Null
                 }
             },
-            cmd => panic!("unimplemented {:?}", cmd),
+            cmd => {
+                println!("What the fuck is this my guy: {:?}", cmd);
+                Frame::Simple("Not OK but whatever niggr".to_string())
+            }
         };
 
         connection.write_frame(&response).await.unwrap();
